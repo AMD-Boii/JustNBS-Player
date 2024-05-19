38c4d448a55171e23e55e13e3e6150fa4efbf334
AMD Boii <zhitnev2002@gmail.com>
2024-05-19 23:03:01 +0700

    Make parser work. Edit gitignore

--- nbs_parser.py
+++ nbs_parser.py
@@ -1,55 +1,86 @@
+from numpy import sign
+
 import pynbs
+import json
+
+
+instrument = (
+    'harp',
+    'bass',
+    'basedrum',
+    'snare',
+    'hat',
+    'guitar',
+    'flute',
+    'bell',
+    'chime',
+    'xylophone',
+    'iron_xylophone',
+    'cow_bell',
+    'didgeridoo',
+    'bit',
+    'banjo',
+    'pling',
+)
+
+pattern = {
+    'sound': 'minecraft:block.note_block.',
+    'pitch': 0,
+    'volume': 0,
+    'variation': '',
+    'source': 'RECORD',
+}
+
+terminator = (
+    0, # tempo
+    1, # show lyrics
+)
 
 
 def parse(nbs_file):
-    #try:
-        nbs = pynbs.read(nbs_file)
-        header = nbs.header
-        notes = nbs.notes
-        layers = nbs.layers
-        #cust_instruments = nbs.instruments
-        last_note = delay = 0
-        delay_seq = pitch_seq = instr_seq = vol_seq = ''
-
-        print(header)
-        pass
+    nbs = pynbs.read(nbs_file)
+    header = nbs.header
+    notes = nbs.notes
+    layers = nbs.layers
+    last_note_id = 0
+    sequence = []
         
-        for tick in range(header.song_length+1):
-            for note_id in range(last_note, len(notes)):
-                last_note = note_id
-                note = notes[note_id]
-                layer = layers[note.layer]
+    for tick in range(header.song_length+1):
+        for note_id in range(last_note_id, len(notes)):
+            last_note_id = note_id
+            note = notes[note_id]
+            layer = layers[note.layer]
                         
-                if note.tick == tick:
-                    if layer.lock: continue
-                    #volume = set_volume(note.velocity, note.panning,
-                    #                    layer.volume, layer.panning)
-                    volume = 100
-                    if volume < 10: continue
-                    pitch = set_pitch(note.key, note.pitch)
-                    if pitch is None: continue
+            if note.tick == tick:
+                if layer.lock: continue
+                volume = set_volume(
+                    note.velocity, layer.volume
+                )
+                if volume <= 0: continue
+                pitch_octave = set_pitch_octave(note.key, note.pitch)
+                if pitch_octave is None: continue
+                panning = set_panning(note.panning, layer.panning)
 
-                    #delay_seq.append(delay)
-                    #print(tick, delay, pitch, volume)
-                    delay = 0
-                else:
-                    delay += 1
-                    break
-
-        with open(nbs_file[:-4]+'.del', 'w') as delay_f:
-            delay_f.write(delay_seq)
-        
-        with open(nbs_file[:-4]+'.pit', 'w') as pitch_f:
-            pitch_f.write(pitch_seq)
-
-        with open(nbs_file[:-4]+'.vol', 'w') as volume_f:
-            volume_f.write(vol_seq)
-        
-        with open(nbs_file[:-4]+'.ins', 'w') as instrument_f:
-            instrument_f.write(instr_seq)
+                element = pattern.copy()
+                element['sound'] = str(
+                    pattern['sound'] +
+                    instrument[note.instrument] +
+                    pitch_octave[1]
+                )
+                element['pitch'] = pitch_octave[0]
+                element['volume'] = 1
+                
+                sequence.append(element)
+                sequence.append(panning)
+            else:
+                sequence.append(terminator[0])
+                break
+    
+    print(len(str(sequence)))
+    return sequence
 
 # Based on https://minecraft.fandom.com/wiki/Note_Block#Notes
-def set_pitch(key, pitch):
+def set_pitch_octave(key, pitch):
     key += pitch * 0.01
     octave_range = 1
     
@@ -68,19 +99,27 @@ def set_pitch(key, pitch):
     else:
         return None
     
-    pitch = 0.5*2**((key-9-octave_range*24)/12)
-    #if octave_range == 1: octave_range = ''
-    #else: octave_range = '_' + str(octave_range - 1)
+    pitch = round(0.5*2**((key-9-octave_range*24)/12), 8)
+    if octave_range == 1: octave_range = ''
+    else: octave_range = '_' + str(octave_range - 1)
     
-    #return [octave_range, pitch]
-    return pitch
+    return (pitch, octave_range)
 
-def set_volume(note_vel, note_pann, layer_vol, layer_pann):
+def set_volume(n_vel, l_vol):
+    vol = round(n_vel / 100 * l_vol, 8)
+    return vol
 
-    volume_L = 1
-    volume_R = 1
+def set_panning(n_pan, l_pan):
+    if n_pan == 0 or l_pan == 0:
+        return round(n_pan, 2)
+    elif n_pan > 0:
+        return round(min(100, n_pan + l_pan), 2)
+    else:
+        return round(max(-100, n_pan + l_pan), 2)
 
-    return [volume_L, volume_R]
 
+data = json.dumps(parse('wethands.nbs'), separators=(',', ':'))
+print(len(str(data)))
 
-parse('Queen — Bohemian Rhapsody.nbs')
+with open('test.json', 'w') as json_result:
+    json_result.write(data)
