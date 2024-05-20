1eb18d8aa2f99f450b9a4af2e0c263f2ec474fbe
AMD Boii <89309022+AMD-Boii@users.noreply.github.com>
2024-05-20 11:36:02 +0700

    Tidy-up the parser

--- nbs_parser.py
+++ nbs_parser.py
@@ -1,9 +1,32 @@
-from numpy import sign
-
 import pynbs
 import json
 
 
+# TEMPO
+tempo = {
+    20.0 : 1,
+    10.0 : 2,
+    6.67 : 3,
+    5.0 : 4,
+    4.0 : 5,
+    3.33 : 6,
+    2.86 : 7,
+    2.5 : 8,
+    2.22 : 9,
+    2.0 : 10,
+    1.82 : 11,
+    1.67 : 12,
+    1.54 : 13,
+    1.43 : 14,
+    1.33 : 15,
+    1.25 : 16,
+    1.18 : 17,
+    1.11 : 18,
+    1.05 : 19,
+    1.0 : 20,
+}
+
+# BASE INSTRUMENTS
 instrument = (
     'harp',
     'bass',
@@ -23,59 +46,67 @@ instrument = (
     'pling',
 )
 
-pattern = [
-    '',
-    0,
-    0,
-]
-
+# SPECIAL TERMINATORS
 terminator = (
     0, # tempo
     1, # show lyrics
 )
 
 
-def parse(nbs_file):
-    nbs = pynbs.read(nbs_file)
-    header = nbs.header
-    notes = nbs.notes
-    layers = nbs.layers
-    last_note_id = 0
-    sequence = []
-        
-    for tick in range(header.song_length+1):
-        for note_id in range(last_note_id, len(notes)):
-            last_note_id = note_id
-            note = notes[note_id]
-            layer = layers[note.layer]
-                        
-            if note.tick == tick:
-                if layer.lock: continue
-                volume = set_volume(
-                    note.velocity, layer.volume
-                )
-                if volume <= 0: continue
-                pitch_octave = set_pitch_octave(note.key, note.pitch)
-                if pitch_octave is None: continue
-                panning = set_panning(note.panning, layer.panning)
 
-                element = pattern.copy()
-                element[0] = str(
-                    instrument[note.instrument] +
-                    pitch_octave[1]
-                )
-                element[1] = pitch_octave[0]
-                element[2] = volume
-                #element[3] = panning
-                
-                sequence.append(element)
-                #sequence.append(panning)
-            else:
-                sequence.append(terminator[0])
-                break
+def parse(nbs_file):
+    '''
+    Parses OpenNBS file that matches with some conditions.\n
+    ONBS version must be 5.\n
+    The tempo must match with the defined tempo dictionary.
+    '''
+
+    try:
+        nbs = pynbs.read(nbs_file)
+        header = nbs.header
+        notes = nbs.notes
+        layers = nbs.layers
+        last_note_id = 0
+        sequence = []
+        
+        assert header.tempo in tempo.keys()
+        
+        for tick in range(header.song_length+1):
+            for note_id in range(last_note_id, len(notes)):
+                last_note_id = note_id
+                note = notes[note_id]
+                layer = layers[note.layer]
+                    
+                if note.tick == tick:
+                    if layer.lock: continue
+                    volume = set_volume(
+                        note.velocity, layer.volume
+                    )
+                    if volume <= 0: continue
+                    pitch_octave = set_pitch_octave(note.key, note.pitch)
+                    if pitch_octave is None: continue
+                    panning = set_panning(note.panning, layer.panning)
+
+                    element = [0, 0, 0, 0]
+                    element[0] = str(
+                        instrument[note.instrument] +
+                        pitch_octave[1]
+                    )
+                    element[1] = pitch_octave[0]
+                    element[2] = volume
+                    element[3] = panning
+                    
+                    sequence.append(element)
+
+                else:
+                    sequence.append(terminator[0])
+                    break
+        
+        print(len(str(sequence)))
+        return sequence
     
-    print(len(str(sequence)))
-    return sequence
+    except Exception as ex:
+        return ex
 
 # Based on https://minecraft.fandom.com/wiki/Note_Block#Notes
 def set_pitch_octave(key, pitch):
@@ -97,14 +128,14 @@ def set_pitch_octave(key, pitch):
     else:
         return None
     
-    pitch = round(0.5*2**((key-9-octave_range*24)/12), 8)
+    pitch = round(0.5*2**((key-9-octave_range*24)/12), 4)
     if octave_range == 1: octave_range = ''
     else: octave_range = '_' + str(octave_range - 1)
     
     return (pitch, octave_range)
 
 def set_volume(n_vel, l_vol):
-    vol = round(n_vel / 10000 * l_vol, 8)
+    vol = round(n_vel / 10000 * l_vol, 2)
     return vol
 
 def set_panning(n_pan, l_pan):
@@ -115,20 +146,29 @@ def set_panning(n_pan, l_pan):
     else:
         return round(max(-100, n_pan + l_pan), 2)
 
-#data = json.dumps(parse('Queen — Bohemian Rhapsody.nbs'), separators=(',', ':'))
-file = 'show.nbs'
-data = []
-final = []
-for value in parse(file):
-    if len(str(data)) < 25000:
-        data.append(value)
-    else:
-        final.append(data)
-        data = []
+def sepparate_data(data):
+    final = []
+    for value in data:
+        if len(str(data).replace(' ', '')) < 24990:
+            data.append(value)
+        else:
+            final.append(data)
+            data = []
 
-i = 0
-for data in final:
-    data = json.dumps(data, separators=(',', ':'))
-    with open(file[:-4]+'_'+str(i)+'.json', 'w') as json_result:
-        json_result.write(data)
-    i += 1
+    return final
+
+def dump_data(data):
+    i = 0
+    for element in data:
+        element = json.dumps(data, separators=(',', ':'))
+        with open(file[:-4]+'_'+str(i)+'.json', 'w') as json_result:
+            json_result.write(data)
+        i += 1
+
+file = 'Queen — Bohemian Rhapsody.nbs'
+data = []
+
+
+
+
+
